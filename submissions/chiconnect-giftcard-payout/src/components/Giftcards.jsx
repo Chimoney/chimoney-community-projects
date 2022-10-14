a49907cd030ae64bf3eb335933381797b49e7678
ezechuka <ezechukwuka9@gmail.com>
2022-10-14 19:25:20 +0100

    feat: Fetch and display list of giftcards

--- submissions/chiconnect-giftcard-payout/src/components/Giftcards.jsx
+++ submissions/chiconnect-giftcard-payout/src/components/Giftcards.jsx
@@ -2,41 +2,44 @@ import { useEffect, useState } from 'react'
 import { getGiftcards } from '../service/fetchApi'
 import Giftcard from './Giftcard'
 
-const Giftcards = () => {
+const Giftcards = ({ handleCardClick }) => {
 
     const [searchTerm, setSearchTerm] = useState('')
     const [giftcards, setGiftcards] = useState([])
     const [originalCards, setOriginalCards] = useState([])
     const [productId, setProductId] = useState(null)
+    const [loading, setLoading] = useState(false)
     const [selected, setSelected] = useState(null)
 
     const handleChange = (event) => {
         const searchQuery = event.target.value
-        console.log(searchQuery)
         setSearchTerm(searchQuery)
         let filteredCards = []
         if (searchQuery.length > 0) {
             filteredCards = originalCards.filter((item) =>
-                item.name.toLowerCase().includes(searchQuery.toLowerCase()))
+                item.name.toLowerCase().startsWith(searchQuery.toLowerCase()))
             setGiftcards(filteredCards)
         } else {
             setGiftcards(originalCards)
         }
     }
 
-    const handleClick = (productId) => {
+    const handleClick = (productId, name) => {
         setProductId(productId)
         setSelected(productId)
+        handleCardClick(productId, name)
     }
 
     useEffect(() => {
         const fetchGiftcards = async () => {
+            setLoading(true)
             const data = await getGiftcards()
             const newData = data.data.benefitsList.filter(
                 (asset) => asset.code === 'giftcard'
             )
             setGiftcards(newData)
             setOriginalCards(newData)
+            setLoading(false)
         }
 
         fetchGiftcards()
@@ -46,21 +49,35 @@ const Giftcards = () => {
         <div className='flex flex-col'>
             <input placeholder='Filter gift cards' onChange={handleChange}
                 value={searchTerm}
-                className='ml-auto mt-3 px-3 py-2 max-w-xs bg-white border shadow-sm 
+                className='ml-auto mt-3 px-3 py-2 mr-3 max-w-xs bg-white border shadow-sm 
                 border-slate-300 focus:outline-none focus:border-purple-500
                 w-full rounded-md sm:text-sm focus:ring-1' />
 
-            <div className='flex flex-row mt-6 h-1/3 justify-between items-start'>
+            <div className='flex flex-row mt-6 justify-between items-start'>
                 <div>
                     <h3 className='text-2xl text-slate-700 font-medium'>
-                        Pay with Gift Card
+                        Send Gift Card
                     </h3>
                     <p className='text-slate-500 text-sm max-w-[250px]'>
-                        Specify what kind of gift card you like to pay to
+                        Select what kind of gift card you want to send
                     </p>
                 </div>
+                
+                {
+                    loading &&
+                    <div className='flex flex-col  ml-72 justify-around items-center'>
+                        <svg className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-300 fill-purple-500" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                        </svg>
 
-                <div className='grid grid-cols-3 gap-6 max-h-10 justify-evenly items-center'>
+                        <p className='text-purple-500 text-center mt-2 text-md font-semibold'>
+                            Fetching Gift Cards
+                        </p>
+                    </div>
+                }
+
+                <div className='grid grid-cols-3 p-3 gap-6 min-h-[150px] max-h-[450px] overflow-y-scroll justify-evenly items-center'>
                     {
                         giftcards.map((item) =>
                             <Giftcard
@@ -68,7 +85,7 @@ const Giftcards = () => {
                                 cardImg={item.logoUrls[0]}
                                 name={item.name}
                                 selected={item.productId === selected ? true : false}
-                                handleClick={() => handleClick(item.productId)}
+                                handleClick={() => handleClick(item.productId, item.name)}
                             />
                         )
                     }
