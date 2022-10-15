a075ee91dcdfad9d44acea5a8b993ef9168b071b
Awe Ayomidipupo <pupoawe@gmail.com>
2022-10-15 15:55:40 +0100

    Add custom errors for the sdk

---
+++ submissions/chimoney-js/Errors.js
@@ -0,0 +1,27 @@
+class ChiMoneyError extends Error {
+  constructor(message = "Chi Money Error") {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
+class ValueError extends ChiMoneyError {
+  constructor(message, errors) {
+    super(message);
+    this.errors = errors;
+    this.name = this.constructor.name;
+  }
+}
+
+class AuthKeyError extends ChiMoneyError {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
+module.exports = {
+  ChiMoneyError,
+  ValueError,
+  AuthKeyError,
+};
