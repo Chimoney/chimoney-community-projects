5325cb843fad0831477e8ddfcc830aadb2cee6e8
ezechuka <ezechukwuka9@gmail.com>
2022-10-17 19:41:01 +0100

    chore: Extend tailwind to include epilogue font

--- submissions/pay-paddy/tailwind.config.cjs
+++ submissions/pay-paddy/tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        epilogue: 'Epilogue, sans-serif'
+      }
+    },
   },
   plugins: [],
 }
