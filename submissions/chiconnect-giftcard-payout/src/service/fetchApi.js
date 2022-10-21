3868cfdb8eab7f81139fcc7342df544e2e2e9f04
ezechuka <ezechukwuka9@gmail.com>
2022-10-21 21:23:28 +0100

    feat: Filter giftcards by country code

--- submissions/chiconnect-giftcard-payout/src/service/fetchApi.js
+++ submissions/chiconnect-giftcard-payout/src/service/fetchApi.js
@@ -1,7 +1,7 @@
 export const API_KEY = `${import.meta.env.VITE_API_KEY}`
 
-export const getGiftcards = async () => { 
-    const response = await fetch('https://api.chimoney.io/v0.2/info/assets', {
+export const getGiftcards = async (countryCode) => {
+    const response = await fetch(`https://api.chimoney.io/v0.2/info/assets?countryCode=${countryCode}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
