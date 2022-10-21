6761d8668e2ec20a92971982eec8e61bd3f31d2c
Adeleke Oluwatobi <tobiadiks@gmail.com>
2022-10-21 11:27:55 +0100

    added map and darkmode style

--- submissions/chimap/components/card/service.card.js
+++ submissions/chimap/components/card/service.card.js
@@ -6,7 +6,7 @@ export default function ServiceCard(props) {
 
 
   return (
-    <div className='  w-full '>
+    <div className={`w-full`}>
       <div className='font-bold text-lg text-center'>{props.country}</div>
       <div className='mt-4 border-t'>
 
@@ -20,7 +20,7 @@ export default function ServiceCard(props) {
             // render filtered data and types
             props.type.map((i, index) => (
               <div key={index}>
-                <div className='font-bold border-l-8 px-2 border-black text-lg my-4'>{i}</div>
+                <div className='font-bold border-l-8 px-2 border-gray-800 text-lg my-4'>{i}</div>
 
                 {filterByType(i, props.data).map((i, index) => (
                   <div className='border-t py-2' key={index}>{i.name}</div>
--- submissions/chimap/components/map/global.map.js
+++ submissions/chimap/components/map/global.map.js
@@ -5,13 +5,14 @@ import countryData from '../../utils/countries.json'
 import ServiceCard from "../card/service.card";
 import wc from 'which-country'
 import { filterByCountry, parseAssetType } from '../../helpers/query'
-
+import CountryList from "country-list-with-dial-code-and-flag"
 
 
 
 const GlobalMap = (props) => {
   const [showPopup, setShowPopup] = useState(false)
   const [lngLat, setlngLng] = useState({ lng: 0, lat: 0 })
+  const [darkMode,setDarkMode]=useState(true)
 
   const countryList = countryData
 
@@ -35,7 +36,8 @@ const GlobalMap = (props) => {
     // wc is an helper that returns country ISO3 from position
     const countryObj = wc([i.lngLat.lng, i.lngLat.lat])
     // filters the country list from util
-    const data = countryList.filter((val) => val.code == countryObj)
+    const data = countryList.filter((val) => val["alpha-3"] == countryObj)
+   
     // if location is not valid set state to empty
     if (!countryObj) {
       setCountry("")
@@ -43,7 +45,9 @@ const GlobalMap = (props) => {
       setFilteredType([])
     }
     else {
-      setCountry(data[0]?.name)
+      const flag=CountryList.findFlag(data[0]["alpha-2"].toLowerCase())
+      console.log(flag)
+      setCountry(`${flag?.flag} ${data[0]?.name}`)
       const filter = filterByCountry(data[0]?.name, benefits)
       setFilteredData(filter)
       setFilteredType(parseAssetType(filter))
@@ -67,20 +71,18 @@ const GlobalMap = (props) => {
       onLoad={props.onload}
       mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_KEY}
       initialViewState={{
-
         zoom: 1,
-
       }}
-      style={{ width: '100%', height: '100%' }}
+      style={{ width: '100%', height: '100%', backgroundColor:darkMode?"black":"whitesmoke" }}
       projection='globe'
-      mapStyle="mapbox://styles/mapbox/streets-v11"
+      mapStyle={darkMode?"mapbox://styles/mapbox/dark-v10":"mapbox://styles/mapbox/streets-v11"}
       onClick={(i) => { setlngLng(i.lngLat); positionChange(i); setShowPopup(true); }}
     >
-      <NavigationControl />
+      <NavigationControl  />
       {showPopup && (
         <Popup className="p-4" longitude={lngLat.lng} latitude={lngLat.lat} anchor="top" onClose={() => setShowPopup(!showPopup)}>
           <div>
-            <ServiceCard country={country} data={filteredData} type={filteredType} status={apiStatus} />
+            <ServiceCard dark={darkMode} country={country} data={filteredData} type={filteredType} status={apiStatus} />
 
 
           </div>
@@ -88,7 +90,7 @@ const GlobalMap = (props) => {
       )
 
       }
-
+<div className={`${darkMode?"text-white":"text-black"} absolute top-4 right-12 z-[9999] cursor-pointer`} onClick={()=>setDarkMode(!darkMode)}>{darkMode?"Light":"Dark"}</div>
 
     </Map>
   )
