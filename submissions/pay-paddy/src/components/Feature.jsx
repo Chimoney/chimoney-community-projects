e37471b0f768cdfca3e63a5a8d45ce44288f9fb0
ezechuka <ezechukwuka9@gmail.com>
2022-10-27 21:44:03 +0100

    feat: Add responsive breakpoints for Feature section

--- submissions/pay-paddy/src/components/Feature.jsx
+++ submissions/pay-paddy/src/components/Feature.jsx
@@ -3,18 +3,19 @@ import { Bank, Cards, DeviceMobile, Phone, Users, UsersThree } from 'phosphor-re
 const Feature = () => {
     return (
         <section className='bg-gradient-to-bl from-purple-50 to-blue-100'>
-            <div className='container mx-auto flex flex-col justify-center items-center px-20 py-14'>
-                <h3 className='font-epilogue font-semibold text-4xl text-slate-900'>
+            <div className='container mx-auto flex flex-col px-5 py-14 justify-center items-center md:px-12 lg:px-20'>
+                <h3 className='font-epilogue font-semibold text-xl text-center text-slate-900 md:text-4xl'>
                     Make frictionless payments now
                 </h3>
-                <p className='font-epilogue text-base text-slate-600 text-center mt-2'>
+                <p className='font-epilogue text-sm text-slate-600 text-center mt-2 md:text-base'>
                     Instantly unlock access to varieties of payment possibilites
                 </p>
 
                 {/* FEATURE DATA */}
-                <div className='grid grid-cols-3 mt-14'>
+                <div className='grid grid-cols-1 mt-4 md:mt-14 md:grid-cols-3'>
                     {/* CELL 1 */}
-                    <div className='flex flex-col justify-between items-center p-8 border-r-2 border-b-2 border-purple-500/20'>
+                    <div className='flex flex-col justify-between items-center p-8 border-b-2 
+                        border-purple-500/20 md:border-r-2'>
                         <Bank weight='duotone'
                             color='hsl(240, 0%, 25%)'
                             size={36}
@@ -31,7 +32,8 @@ const Feature = () => {
                     </div>
 
                     {/* CELL 2 */}
-                    <div className='flex flex-col justify-between items-center p-8 border-r-2 border-b-2 border-purple-500/20'>
+                    <div className='flex flex-col justify-between items-center p-8 border-b-2
+                     border-purple-500/20 md:border-r-2'>
                         <Phone weight='duotone'
                             color='hsl(240, 0%, 25%)'
                             size={36}
@@ -65,7 +67,8 @@ const Feature = () => {
                     </div>
 
                     {/* CELL 4 */}
-                    <div className='flex flex-col justify-between items-center p-8 border-r-2 border-purple-500/20'>
+                    <div className='flex flex-col justify-between items-center p-8 border-b-2 border-purple-500/20
+                        md:border-r-2 md:border-b-0'>
                         <Cards weight='duotone'
                             color='hsl(240, 0%, 25%)'
                             size={36}
@@ -82,7 +85,8 @@ const Feature = () => {
                     </div>
 
                     {/* CELL 5 */}
-                    <div className='flex flex-col justify-between items-center p-8 border-r-2 border-purple-500/20'>
+                    <div className='flex flex-col justify-between items-center p-8 border-b-2 border-purple-500/20
+                        md:border-r-2 md:border-b-0'>
                         <Users weight='duotone'
                             color='hsl(240, 0%, 25%)'
                             size={36}
@@ -97,7 +101,7 @@ const Feature = () => {
                             Transfer payment to another paypaddy user
                         </p>
                     </div>
-                    
+
                     {/* CELL 6 */}
                     <div className='flex flex-col justify-between items-center p-8 border-purple-500/20'>
                         <UsersThree weight='duotone'
