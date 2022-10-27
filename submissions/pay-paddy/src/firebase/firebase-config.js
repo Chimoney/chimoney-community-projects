8f20d76a9d5d5e408e774350cf931222774a0cc4
ezechuka <ezechukwuka9@gmail.com>
2022-10-27 13:51:30 +0100

    chore: Add firebase config

--- submissions/pay-paddy/src/firebase/firebase-config.js
+++ submissions/pay-paddy/src/firebase/firebase-config.js
@@ -1,14 +1,19 @@
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyBOmiufvwG5kjblabL3g5K7p-3bg-VRy7A",
-  authDomain: "pay-paddy.firebaseapp.com",
-  projectId: "pay-paddy",
-  storageBucket: "pay-paddy.appspot.com",
-  messagingSenderId: "345830743572",
-  appId: "1:345830743572:web:b5a716fb5b429720665101",
-  measurementId: "G-X889Y7VRF3"
+
+  apiKey: "AIzaSyCI0hRFvRlLgrbNUg8R5jyBhpLe0egvZq8",
+
+  authDomain: "pay-paddy-c9c1a.firebaseapp.com",
+
+  projectId: "pay-paddy-c9c1a",
+
+  storageBucket: "pay-paddy-c9c1a.appspot.com",
+
+  messagingSenderId: "528655011976",
+
+  appId: "1:528655011976:web:a409cf6d9a87dafdfd8f3d"
+
 };
