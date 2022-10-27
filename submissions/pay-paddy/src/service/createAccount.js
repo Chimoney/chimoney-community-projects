7aa377b307a63087bc008e339ebab69ed6050c6f
ezechuka <ezechukwuka9@gmail.com>
2022-10-27 13:53:30 +0100

    feat: Implement create-account api call

---
+++ submissions/pay-paddy/src/service/createAccount.js
@@ -0,0 +1,18 @@
+const API_KEY = `${import.meta.env.VITE_API_KEY}`
+
+export const createAccount = async (name, email) => {
+    const response = await fetch('https://api.chimoney.io/v0.2/sub-account/create', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-API-KEY': API_KEY
+        },
+        body: JSON.stringify({
+            'name': name,
+            'email': email
+        })
+    })
+
+    const data = await response.json()
+    return data
+}
