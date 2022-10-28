19676763cca399cfd75997ab3f7dbad8cc422dea
ezechuka <ezechukwuka9@gmail.com>
2022-10-28 19:32:32 +0100

    fix: Import key from process.env

--- submissions/pay-paddy/src/service/createAccount.js
+++ submissions/pay-paddy/src/service/createAccount.js
@@ -1,4 +1,4 @@
-const API_KEY = `${import.meta.env.VITE_API_KEY}`
+const API_KEY = `${process.env.API_KEY}`
 
 export const createAccount = async (name, email) => {
     const response = await fetch('https://api.chimoney.io/v0.2/sub-account/create', {
