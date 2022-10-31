93e5ccf2e8a4588db58ad8832d756dafbe75b02d
Asikhalaye Samuel <sammyboy.as@gmail.com>
2022-10-31 23:51:53 +0100

    payout function

--- submissions/Chimoney-Slackbot/utils/functions.py
+++ submissions/Chimoney-Slackbot/utils/functions.py
@@ -1,3 +1,7 @@
+import os
+from chimoney import Chimoney
+
+
 def get_user_email(client, user_id):
     user = client.web_client.users_info(user=user_id)
     return user["user"]["profile"]["email"]
@@ -13,8 +17,47 @@ def is_mention(client, event, bot_id=None):
     return event["text"].startswith(f"<@{bot_id}>")
 
 
-def send_chimoney(client, user_ids, amount):
-    emails = []
-    for user_id in user_ids:
-        emails.append(get_user_email(client, user_id))
-    pass
+def is_valid_command(client, event, bot_id=None):
+    if not bot_id:
+        bot_id = get_bot_id(client)
+    return event["text"].startswith(f"<@{bot_id}> send")
+
+
+def send_chimoney(client, text, amount):
+    # Split the tweet content
+    tweet_content = text.split()
+
+    # Get the amount and recipients
+    amount = tweet_content[2]
+
+    # remove <@ and > from the string
+    if "to" in tweet_content:
+        recipients = [recipient[2:-1] for recipient in tweet_content[4:]]
+    recipients = [recipient[2:-1] for recipient in tweet_content[3:]]
+
+    # Get the recipients
+    if "to" in tweet_content:
+        recipients = tweet_content[tweet_content.index("to") + 1 :]
+    else:
+        recipients = tweet_content[3:]
+
+    checkout_value = []
+    for user_id in recipients:
+        temp = {
+            "email": get_user_email(client, user_id),
+            "amount": amount,
+        }
+        checkout_value.append(temp)
+
+    chimoney = Chimoney.set_api_key(os.getenv("CHIMONEY_API_KEY"))
+    task_status = chimoney.payouts.initiate_chimoney(checkout_value)
+
+    if task_status[1] == 200:
+        # Get the payout link from the task status
+        payment_link = task_status[0]["data"]["paymentLink"]
+        chiRef = task_status[0]["data"]["data"][0]["id"]
+
+        response = {"payment_link": payment_link, "chiRef": chiRef}
+        return response
+    else:
+        return None
--- submissions/Chimoney-Slackbot/utils/rtm.py
+++ submissions/Chimoney-Slackbot/utils/rtm.py
@@ -3,6 +3,7 @@ from slack_sdk.rtm_v2 import RTMClient
 from dotenv import load_dotenv
 import logging
 import multiprocessing
+from functions import is_valid_command, send_chimoney, is_mention
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -11,6 +12,23 @@ load_dotenv()
 
 def start_rtm(token):
     rtm_client = RTMClient(token=token)
+
+    @rtm_client.on("message")
+    def print(client: RTMClient, event: dict):
+        # send a messenge to the user direct message
+        if is_valid_command(client, event):
+            response = send_chimoney(client, event["user"])
+            if response:
+                client.web_client.chat_postMessage(
+                    channel=event["channel"],
+                    text=f"Payment link: {response['payment_link']}",
+                )
+            else:
+                client.web_client.chat_postMessage(
+                    channel=event["channel"],
+                    text=f"Something went wrong, please try again later",
+                )
+
 
     @rtm_client.on("message")
     def handle(client: RTMClient, event: dict):
