846389b56c5156787f7dbc73ac4869e7cef11941
Awe Ayomidipupo <pupoawe@gmail.com>
2022-11-22 20:02:44 +0100

    chore: add morgan for logging

--- submissions/chimoney-discord-bot/app.js
+++ submissions/chimoney-discord-bot/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 const webhookRouter = require("./routes/webhooks");
 
 // Router handlers
 app.use("/webhooks", webhookRouter);
+app.use(morgan("tiny"));
 
 module.exports = app;
