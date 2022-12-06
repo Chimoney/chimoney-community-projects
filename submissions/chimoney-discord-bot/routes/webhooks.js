3d148da9deff4fa64164b81ceff4ba652aee95c8
Awe Ayomidipupo <pupoawe@gmail.com>
2022-12-06 14:26:15 +0100

    chore: remove web server related code

--- submissions/chimoney-discord-bot/routes/webhooks.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-
-const bodyparser = require("body-parser");
-const { handleWebhook } = require("../controllers/webhook.controller");
-
-router.post("/", bodyparser.raw({ type: "application/json" }), handleWebhook);
-
-module.exports = router;
