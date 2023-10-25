f26148afed00a8db8b0b8025b08910761259f257
Aryan <117700812+gitsofaryan@users.noreply.github.com>
2023-10-25 16:29:24 +0530

    Develop An Voice InterPretation Bot for Chimoney Website

---
+++ submissions/Chimoney-Python/chimoney/voice_interpretation_bot.py
@@ -0,0 +1,27 @@
+import speech_recognition as sr
+from googletrans import Translator
+
+# Initialize the recognizer
+r = sr.Recognizer()
+translator = Translator()
+
+# Function to interpret speech
+def interpret_speech():
+    with sr.Microphone() as source:
+        print("Speak something...")
+        audio = r.listen(source)
+
+        try:
+            text = r.recognize_google(audio)
+            print(f"Recognized: {text}")
+            translation = translator.translate(text, dest='en')
+            print(f"Translation: {translation.text}")
+
+        except sr.UnknownValueError:
+            print("Could not understand audio")
+
+        except sr.RequestError as e:
+            print(f"Could not request results; {e}")
+
+# Call the function
+interpret_speech()
