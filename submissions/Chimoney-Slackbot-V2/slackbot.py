3612e0149ccff84812cded8e05ad26ab478f0dd4
thelimeskies <sammyboy.as@gmail.com>
2023-10-29 14:03:23 +0100

    Temp Use of FileStore

--- submissions/Chimoney-Slackbot-V2/slackbot.py
+++ submissions/Chimoney-Slackbot-V2/slackbot.py
@@ -1,13 +1,12 @@
 from slack_bolt.oauth.async_oauth_settings import AsyncOAuthSettings
 from slack_bolt.async_app import AsyncApp
 import os
-from slack_sdk.oauth.installation_store import sqlalchemy
+from slack_sdk.oauth.installation_store import FileInstallationStore
 from slack_sdk.oauth.state_store import FileOAuthStateStore
 from modules.validators import validate_sendchimoney_text
 from sqlalchemy import create_engine
 from modules.sendchimoney import (
     send_chimoney as send_chimoney_response,
-    get_user_email_from_username,
 )
 import logging
 
@@ -18,17 +17,17 @@ import os
 
 load_dotenv()
 
-installation_store = sqlalchemy.SQLAlchemyInstallationStore(
-    client_id=os.environ["CLIENT_ID"],
-    engine=create_engine(os.environ["DATABASE_URL"]),
-    logger=logger,
-)
+# installation_store = sqlalchemy.SQLAlchemyInstallationStore(
+#     client_id=os.environ["CLIENT_ID"],
+#     engine=create_engine(os.environ["DATABASE_URL"]),
+#     logger=logger,
+# )
 
 # Initializes your app with your bot token and signing secret
 SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
 CLIENT_ID = os.environ["CLIENT_ID"]
 CLIENT_SECRET = os.environ["CLIENT_SECRET"]
-# OAUTH_REDIRECT_URI = os.environ["OAUTH_REDIRECT_URI"]
+OAUTH_REDIRECT_URI = os.environ["OAUTH_REDIRECT_URI"]
 
 
 # Initializes your Slack Client and Bolt App
@@ -39,10 +38,11 @@ app = AsyncApp(
         client_id=CLIENT_ID,
         client_secret=CLIENT_SECRET,
         scopes=["channels:read", "groups:read", "chat:write"],
-        installation_store=installation_store,
+        installation_store=FileInstallationStore(base_dir="./data"),
         state_store=FileOAuthStateStore(
             expiration_seconds=600, base_dir="./data/oauth_states"
         ),
+        redirect_uri=OAUTH_REDIRECT_URI,
     ),
 )
 
@@ -52,28 +52,6 @@ async def send_chimoney(ack, say, command, client, logger):
     await ack()
     logger.info(command)
     try:
-        # user_info = await client.users_info(user=command["user_id"])
-        # user_email = user_info.data["user"]["profile"]["email"]
-        # await say(f"Hey there {user_email}!")
-
-        # # send private message to user
-        # await client.chat_postMessage(
-        #     channel=command["user_id"],
-        #     text="This is a test message from your app! :tada:",
-        # )
-        # text = command["text"]
-        # email = await get_user_email_from_username(client, text, command["team_id"])
-        # if email:
-        #     await client.chat_postMessage(
-        #         channel=command["user_id"],
-        #         text="Email: " + email,
-        #     )
-        # else:
-        #     await client.chat_postMessage(
-        #         channel=command["user_id"],
-        #         text="An error occurred while processing your request.",
-        #     )
-
         text = command["text"]
 
         valid, text_type = validate_sendchimoney_text(text)
